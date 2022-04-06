498525f0ccc00459252c12d359ac7f40ef480aa8
Seokyong Shin <kistssy@gmail.com>
2022-04-06 10:44:02 +0900

    Change prompt

--- convert_png_to_jpg.py
+++ convert_png_to_jpg.py
@@ -9,7 +9,7 @@ for image_path in tqdm.tqdm(image_paths, 'Convert images'):
     image = torchvision.io.read_image(image_path)
     torchvision.io.write_jpeg(image, image_path.replace('.png', '.jpg'), quality=100)
 
-if input('원본 이미지를 삭제할까요? (y/[n])') == 'y':
+if input('원본 이미지를 삭제할까요? (y/[n]) ') == 'y':
     for image_path in tqdm.tqdm(image_paths, 'Delete original images'):
         os.remove(image_path)
 else:
