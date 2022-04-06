4e81ac995a62ca147cc3374e237a3ec39e19a7b6
Seokyong Shin <kistssy@gmail.com>
2022-04-06 10:42:57 +0900

    기능 추가

--- convert_png_to_jpg.py
+++ convert_png_to_jpg.py
@@ -1,9 +1,16 @@
 import os
 import glob
 import torchvision
+import tqdm
 
 image_paths = glob.glob(os.path.join('data', '*.png'))
 
-for image_path in image_paths:
+for image_path in tqdm.tqdm(image_paths, 'Convert images'):
     image = torchvision.io.read_image(image_path)
     torchvision.io.write_jpeg(image, image_path.replace('.png', '.jpg'), quality=100)
+
+if input('원본 이미지를 삭제할까요? (y/[n])') == 'y':
+    for image_path in tqdm.tqdm(image_paths, 'Delete original images'):
+        os.remove(image_path)
+else:
+    print('삭제 안함.')
