2c77a573df4dd9f8a9f29dcb7d7850e02e649b93
Seokyong Shin <kistssy@gmail.com>
2022-04-08 17:36:45 +0900

    utils: Use float32 in torchvision

--- utils.py
+++ utils.py
@@ -39,7 +39,7 @@ class AugmentationDataset(torch.utils.data.Dataset):
             image = self.transform(image)
             augmentation_time = time.time() - augmentation_time
         elif self.augmentation_api == 'torchvision':
-            image = torch.as_tensor(image).permute((2, 0, 1))
+            image = torch.as_tensor(image).permute((2, 0, 1)).to(torch.float32)
             augmentation_time = time.time()
             image = self.transform(image)
             augmentation_time = time.time() - augmentation_time
